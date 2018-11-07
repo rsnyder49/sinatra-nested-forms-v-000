073c58104fd1977ddc4b8aca1840b4feb1490098
Robert Snyder <r.snyder49@gmail.com>
2018-11-07 21:56:18 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,16 @@
 class Ship
+  attr_accessor :name, :type, :booty 
+  
+  @@all = [] 
+  
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all
+  end 
 end
