f66f623fe75f373a2f00bb87d008465ea07768de
Robert Snyder <r.snyder49@gmail.com>
2018-11-07 22:10:45 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ module FormsLab
     post '/pirates' do 
       @pirate = Pirate.new(params[:pirate][:name], params[:pirate][:weight], params[:pirate][:height])
       params[:pirate][:ships].each do |details|
-        @ship
+        Ship.new()
       
       erb :"pirates/show"
     end
